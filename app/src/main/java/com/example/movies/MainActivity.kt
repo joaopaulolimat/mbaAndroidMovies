c1f2f2803ab624f95a9a0eab54748ee84f14255f
João Paulo <jp_lt01@hotmail.com>
2023-10-05 15:43:57 -0300

    Projeto de estudo em Android

---
+++ app/src/main/java/com/example/movies/MainActivity.kt
@@ -0,0 +1,81 @@
+package com.example.registercontacts
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.Toast
+import com.example.movies.R
+import com.example.movies.databinding.ActivityMainBinding
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var auth: FirebaseAuth
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+        auth = Firebase.auth
+
+
+        setUpListener()
+
+
+    }
+
+    public override fun onStart() {
+        super.onStart()
+//        val currentUser = auth.currentUser
+//        if (currentUser != null) {
+//            val intent = Intent(this, Contacts::class.java)
+//            startActivity(intent)
+//        }
+    }
+
+    private fun setUpListener() {
+//        binding.txtSignUp.setOnClickListener {
+//            val intent = Intent(this, SignUp::class.java)
+//            startActivity(intent)
+//        }
+
+        binding.btnEntrar.setOnClickListener(View.OnClickListener {
+
+            val email = binding.txtEmail.text.toString()
+            val password = binding.txtPassword.text.toString()
+
+            if (password.isEmpty() || email.isEmpty()) {
+                Toast.makeText(
+                    baseContext, getString(R.string.fields_for_login),
+                    Toast.LENGTH_SHORT
+                ).show()
+
+                return@OnClickListener
+            }
+
+            auth.signInWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this) { task ->
+                    if (task.isSuccessful) {
+                        auth.currentUser
+                        Toast.makeText(
+                            baseContext, getString(R.string.login_done),
+                            Toast.LENGTH_SHORT
+                        ).show()
+//                        val intent = Intent(this, Contacts::class.java)
+                        startActivity(intent)
+                    } else {
+                        // If sign in fails, display a message to the user.
+                        Toast.makeText(
+                            baseContext, getString(R.string.login_failed),
+                            Toast.LENGTH_SHORT
+                        ).show()
+                    }
+                }
+        })
+    }
+
+
+}
---
+++ app/src/main/java/com/example/movies/View/Movies.kt
@@ -0,0 +1,37 @@
+package com.example.movies.View
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import com.example.movies.AddMovieFragment
+import com.example.movies.MoviesFragment
+import com.example.movies.R
+import com.example.movies.databinding.ActivityMoviesBinding
+
+class Movies : AppCompatActivity() {
+    private lateinit var binding: ActivityMoviesBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_movies)
+        binding = ActivityMoviesBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+        replaceFragment(MoviesFragment())
+        binding.bottomNavigationView.setOnNavigationItemSelectedListener {
+            when (it.itemId) {
+                R.id.movies -> replaceFragment(MoviesFragment())
+                R.id.add_movie -> replaceFragment(AddMovieFragment())
+                else -> {}
+            }
+            true
+        }
+    }
+
+    private fun replaceFragment(fragment: Fragment) {
+        val fragmentManager = supportFragmentManager
+        val fragmentTransaction = fragmentManager.beginTransaction()
+        fragmentTransaction.replace(R.id.frame_layout, fragment)
+        fragmentTransaction.commit()
+    }
+}
